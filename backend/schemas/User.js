b0a702db90041c9b0604a066bd748aa6acf7ecc4
gabrieleal-beep <gabriel.leal@impulsum.me>
2025-09-19 11:09:47 -0300

    some fix

--- backend/schemas/User.js
+++ backend/schemas/User.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     phone: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       validate: {
         isNumeric: false
       }
