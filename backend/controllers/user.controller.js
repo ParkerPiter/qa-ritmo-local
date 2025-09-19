7d7466fdb371b010df84292cd71e0aab629b0614
gabrieleal-beep <gabriel.leal@impulsum.me>
2025-09-19 15:55:37 -0300

    test2

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -18,12 +18,13 @@ async function getAllUsers(req, res) {
 async function createUser(req, res) {
     const { email, fullName, password } = req.body;
     try {
-        const existingUser = await User.findOne({ where: { email } });
+        const userModel = User(sequelize, Sequelize.DataTypes);
+        const existingUser = await userModel.findOne({ where: { email } });
         if (existingUser) {
             return res.status(400).send('El email ya está en uso');
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ email, password: hashedPassword, fullName });
+        const user = await userModel.create({ email, password: hashedPassword, fullName });
         const userResponse = {
         id: user.id,
         email: user.email,
