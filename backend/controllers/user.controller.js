0975a2ba96a75d15cdfdc1f31713db315ecbfc21
gabrieleal-beep <gabriel.leal@impulsum.me>
2025-09-19 11:35:00 -0300

    fix in create user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -18,10 +18,21 @@ async function getAllUsers(req, res) {
 async function createUser(req, res) {
     const { email, fullName, password } = req.body;
     try {
-      const UserModel = User(sequelize, Sequelize.DataTypes);
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await UserModel.create({ email, password: hashedPassword, fullName });
-      res.status(201).send('Usuario creado exitosamente');
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).send('El email ya está en uso');
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await UserModel.create({ email, password: hashedPassword, fullName });
+        const userResponse = {
+        id: user.id,
+        email: user.email,
+        fullName: user.fullName
+      };
+        res.status(201).json({
+          message: 'Usuario creado exitosamente',
+          user: userResponse
+        });
     } catch (error) {
       res.status(500).send(error.message);
     }
