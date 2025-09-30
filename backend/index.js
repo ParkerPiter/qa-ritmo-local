227a8863992ee883d9e514243f907182cdbffc3f
Gabriel Alejandro Leal <68304965+ParkerPiter@users.noreply.github.com>
2025-09-30 11:25:50 -0300

    Force recreate table if it is empty or new
    Changed model synchronization to force table recreation.

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,14 @@ const checkAndSyncTable = async (model, modelName) => {
   if (tableExists) {
     const count = await model.count();
     if (count === 0) {
-      await model.sync({ force: false});
+      await model.sync({ force: true});
       console.log(`Table ${modelName} recreated because it was empty.`);
     } else {
       await model.sync();
       console.log(`Table ${modelName} already exists and has data.`);
     }
   } else {
-    await model.sync({ force: false});
+    await model.sync({ force: true});
     console.log(`Table ${modelName} created.`);
   }
 };
