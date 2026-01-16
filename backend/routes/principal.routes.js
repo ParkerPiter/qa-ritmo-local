ca189b02c806e97a1143e4181403963398c7415b
gabrieleal-beep <gabriel.leal@impulsum.me>
2026-01-16 12:48:51 -0300

    fix routes

--- backend/routes/principal.routes.js
+++ backend/routes/principal.routes.js
@@ -6,11 +6,13 @@ const organizador = require('./organizador.routes');
 const admin = require('./admin.routes');
 const verificacion = require('./mail.routes');
 const eventos = require('./eventos.routes');
+const payments = require('./payments.routes');
 
 router.use('/user', user);
 router.use('/organizador', organizador);
 router.use('/admin-user', admin);
 router.use('/verificacion', verificacion);
 router.use('/eventos', eventos);
+router.use('/payments', payments);
 
 module.exports = router;
